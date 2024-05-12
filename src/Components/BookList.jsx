f90516d2b55ed29882e1484ded3c84d56cabc753
MartinNikStarter <marto0323@abv.bg>
2024-05-12 12:37:00 +0300

    Completed

---
+++ src/Components/BookList.jsx
@@ -0,0 +1,20 @@
+const BookList = ({ books, onDelete }) => {
+    return (
+      <ul className="content-list">
+        {books.map(book => (
+          <li key={book.id}>
+            <p className="id">ID: {book.id}</p>
+            <p className="field1">Title: {book.title}</p>
+            <p className="field2">Author: {book.author}</p>
+            <p className="field3">ISBN: {book.isbn}</p>
+            <p className="field4">Price: {book.price}</p>
+            <p className="field5">Publication Date: {book.publicationDate}</p>
+            <button className="deleteButton" onClick={() => onDelete(book.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
+  export default BookList;
+  
