df6b0158dee6b466088507332e512064afcc574f
MartinNikStarter <marto0323@abv.bg>
2024-05-12 12:48:05 +0300

    Completed

--- src/Components/BookForm.jsx
+++ src/Components/BookForm.jsx
@@ -1,33 +1,68 @@
-import React from "react"
+import React from "react";
 
-function BookForm({ onSave, clearForm, selectedBook }) {
-    const [formData, setFormData] = useState(selectedBook || {});
-  
-    function handleChange(e) {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    }
-  
-    function handleSubmit(e) {
-      e.preventDefault();
-      onSave(formData);
-      clearForm();
-    }
-  
-    return (
-      <div className="content-details">
-        <form onSubmit={handleSubmit}>
-          <input type="text" id="field1" name="title" placeholder="Title" value={formData.title || ''} onChange={handleChange} />
-          <input type="text" id="field2" name="author" placeholder="Author" value={formData.author || ''} onChange={handleChange} />
-          <input type="text" id="field3" name="isbn" placeholder="ISBN" value={formData.isbn || ''} onChange={handleChange} />
-          <input type="text" id="field4" name="price" placeholder="Price" value={formData.price || ''} onChange={handleChange} />
-          <input type="text" id="field5" name="publicationDate" placeholder="Publication Date" value={formData.publicationDate || ''} onChange={handleChange} />
-          <button type="submit" id="saveButton" className="btn saveButton">Save</button>
-          <button type="button" id="clearButton" onClick={clearForm} className="btn clearButton">Clear</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default BookForm;
-  
+function BookDetails({
+  selectedBook,
+  handleInputChange,
+  handleSave,
+  handleClear,
+}) {
+  return (
+    <section className="content-details">
+      <h2>Book Details</h2>
+      <form>
+        <input
+          type="text"
+          id="field1"
+          name="title"
+          value={selectedBook ? selectedBook.title : newBook.title}
+          onChange={handleInputChange}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          id="field2"
+          name="author"
+          value={selectedBook ? selectedBook.author : newBook.author}
+          onChange={handleInputChange}
+          placeholder="Author"
+        />
+        <input
+          type="text"
+          id="field3"
+          name="isbn"
+          value={selectedBook ? selectedBook.isbn : newBook.isbn}
+          onChange={handleInputChange}
+          placeholder="ISBN"
+        />
+        <input
+          type="text"
+          id="field4"
+          name="price"
+          value={selectedBook ? selectedBook.price : newBook.price}
+          onChange={handleInputChange}
+          placeholder="Price"
+        />
+        <input
+          type="date"
+          id="field5"
+          name="publicationDate"
+          value={
+            selectedBook
+              ? selectedBook.publicationDate
+              : newBook.publicationDate
+          }
+          onChange={handleInputChange}
+          placeholder="Publication Date"
+        />
+        <button type="button" id="saveButton" onClick={handleSave}>
+          Save
+        </button>
+        <button type="button" id="clearButton" onClick={handleClear}>
+          Clear
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default BookDetails;
