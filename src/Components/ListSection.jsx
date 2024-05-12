f392f42be07555a9286891ec58389de97fb99431
MartinNikStarter <marto0323@abv.bg>
2024-05-12 13:17:50 +0300

    Completed

--- src/Components/ListSection.jsx
+++ src/Components/ListSection.jsx
@@ -5,12 +5,14 @@ function ListSection({ books, selectedBook, onBookSelect, onDeleteBook }) {
     <div className="content-list">
       <ul>
         {books.map(book => (
-          <li key={book.id}>
+          <li key={book.id} onClick={() => onBookSelect(book)}>
             <p className="id">{book.id}</p>
             <p className="field1">{book.title}</p>
             <p className="field2">{book.author}</p>
-            {/* Repeat for other fields */}
-            <button className="deleteButton" onClick={() => onDeleteBook(book.id)}>Delete</button>
+            <p className="field3">{book.isbn}</p>
+            <p className="field4">{book.price}</p>
+            <p className="field5">{book.publicationDate}</p>
+            <button className="deleteButton" onClick={(e) => {e.stopPropagation(); onDeleteBook(book.id)}}>Delete</button>
           </li>
         ))}
       </ul>
