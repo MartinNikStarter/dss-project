31ab50c58a84c8dc2d10e70789b6cbd123dd25c9
MartinNikStarter <marto0323@abv.bg>
2024-05-12 12:22:13 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-import "./App.css";
 
 function App() {
-	return <p>Hello World</p>;
-}
-
-export default App;
+	const [books, setBooks] = useState([]);
+	const [selectedBook, setSelectedBook] = useState(null);
+  
+	function handleSave(formData) {
+	  if (selectedBook) {
+		const updatedBooks = books.map(book => {
+		  if (book.id === selectedBook.id) {
+			return { ...book, ...formData };
+		  }
+		  return book;
+		});
+		setBooks(updatedBooks);
+	  } else {
+		setBooks([...books, { id: books.length + 1, ...formData }]);
+	  }
+	  setSelectedBook(null);
+	}
+  
+	function handleDelete(id) {
+	  const updatedBooks = books.filter(book => book.id !== id);
+	  setBooks(updatedBooks);
+	  setSelectedBook(null);
+	}
+  
+	function clearForm() {
+	  setSelectedBook(null);
+	}
+  
+	return (
+	  <div className="App">
+		<Navbar />
+		<section>
+		  <h2>List Section</h2>
+		  <BookList books={books} onDelete={handleDelete} />
+		</section>
+		<section>
+		  <h2>Details Section</h2>
+		  <BookForm onSave={handleSave} clearForm={clearForm} selectedBook={selectedBook} />
+		</section>
+		<Footer />
+	  </div>
+	);
+  }
+  
+  export default App;
+  
