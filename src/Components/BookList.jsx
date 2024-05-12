027744041e1b16d269f955b463e23a0b1ec7264f
MartinNikStarter <marto0323@abv.bg>
2024-05-12 12:52:54 +0300

    Completed

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,22 +1,29 @@
-import React from "react"
+import React from "react";
 
-const BookList = ({ books, onDelete }) => {
-    return (
-      <ul className="content-list">
-        {books.map(book => (
-          <li key={book.id}>
+function BookList({ books, handleSelectBook, handleDelete }) {
+  return (
+    <section className="content-list">
+      <h2>List of Books</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id} onClick={() => handleSelectBook(book)}>
             <p className="id">ID: {book.id}</p>
             <p className="field1">Title: {book.title}</p>
             <p className="field2">Author: {book.author}</p>
             <p className="field3">ISBN: {book.isbn}</p>
             <p className="field4">Price: {book.price}</p>
             <p className="field5">Publication Date: {book.publicationDate}</p>
-            <button className="deleteButton" onClick={() => onDelete(book.id)}>Delete</button>
+            <button
+              className="deleteButton"
+              onClick={() => handleDelete(book.id)}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
-    );
-  }
-  
-  export default BookList;
-  
+    </section>
+  );
+}
+
+export default BookList;
